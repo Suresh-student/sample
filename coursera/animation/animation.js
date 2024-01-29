c7b712b893509abc5c71c79a3fceb067d44680a4
Suresh-student <157983010+Suresh-student@users.noreply.github.com>
2024-01-29 08:35:36 +0530

    coursera files

---
+++ coursera/animation/animation.js
@@ -0,0 +1,25 @@
+let canvas = document.getElementById("my_canvas");
+let context = canvas.getContext("2d");
+
+
+const speed = 4;
+let position = 0;
+let movespeed = speed;
+let radius=40;
+
+
+
+function moveBall(){
+
+    if(position + radius > 640 ){
+        movespeed= -speed;
+        
+    }
+    else if(position - radius < 0){
+        movespeed = speed;
+    }
+    position += movespeed;
+}
+function drawBall(){
+     
+}
